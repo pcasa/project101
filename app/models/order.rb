c81a3022defc1bdf99f495e38af446aef6e601b1
Peter Casanova <peter.casanova@comcast.net>
2011-04-10 12:43:21 -0400

    fixed some layouts

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,6 @@
 class Order < ActiveRecord::Base
+  cattr_reader :per_page
+    @@per_page = 10
   
   belongs_to :customer, :class_name => "Customer", :foreign_key => "customer_id"
   belongs_to :user, :class_name => "User", :foreign_key => "user_id"
