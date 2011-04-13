e67ef969d467e4c776a3ee89ffd45b6c5539f55b
Peter Casanova <peter.casanova@comcast.net>
2011-04-13 15:00:04 -0400

    migration to fix heroku pkey errors

--- db/migrate/20110413185651_reset_catagories_pkes.rb
+++ db/migrate/20110413185651_reset_catagories_pkes.rb
@@ -1,13 +1,13 @@
 class ResetCatagoriesPkes < ActiveRecord::Migration
   def self.up
-    remove_index(:categories, [:name, :parent_id, :lft, :rgt, :depth], :name => "add_index_to_categories_n_pi_l_r_dpth")
+    remove_index(:categories, [:name, :parent_id, :lft, :rgt, :depth])
     add_index(:categories, [:name, :parent_id, :lft, :rgt, :depth], :name => "add_index_to_categories_n_pi_l_r_dpth")
     remove_index(:companies, [:name, :subdomain])
     add_index(:companies, [:name, :subdomain])
   end
 
   def self.down
-    remove_index(:categories, [:name, :parent_id, :lft, :rgt, :depth], :name => "add_index_to_categories_n_pi_l_r_dpth")
+    remove_index(:categories, [:name, :parent_id, :lft, :rgt, :depth])
     add_index(:categories, [:name, :parent_id, :lft, :rgt, :depth], :name => "add_index_to_categories_n_pi_l_r_dpth")
     remove_index(:companies, [:name, :subdomain])
     add_index(:companies, [:name, :subdomain])
