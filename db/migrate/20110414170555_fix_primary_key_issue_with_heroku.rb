91aabe179953f079a878a59f267639b03cbf5c27
Peter Casanova <peter.casanova@comcast.net>
2011-04-14 13:07:42 -0400

    heroku thing

--- db/migrate/20110414170555_fix_primary_key_issue_with_heroku.rb
+++ db/migrate/20110414170555_fix_primary_key_issue_with_heroku.rb
@@ -1,6 +1,6 @@
 class FixPrimaryKeyIssueWithHeroku < ActiveRecord::Migration
   def self.up
-    execute(SELECT setval('category_id_seq'::regclass, MAX(id)) FROM category;)
+    execute 'SELECT setval('category_id_seq'::regclass, MAX(id)) FROM category;'
   end
 
   def self.down
