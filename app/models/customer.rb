87d855faa18b0b397cf998300b44f74029289f88
Peter Casanova <peter.casanova@comcast.net>
2011-04-26 13:45:09 -0400

    force first and last name to be upcase

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -30,7 +30,13 @@ class Customer < ActiveRecord::Base
   
   include ActiveModel::Dirty
 
+  def firstname=(firstname)
+     write_attribute(:firstname, firstname.upcase)
+   end
   
+   def lastname=(lastname)
+      write_attribute(:lastname, lastname.upcase)
+    end
   
   def make_customer_number(size = 8)
     charset = %w{ 2 3 4 6 7 9 A C D E F G H J K L M N P Q R T V W X Y Z}
