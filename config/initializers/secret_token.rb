23bbce0b760d5294980b625011b3e681c285734b
Peter Casanova <peter.casanova@comcast.net>
2015-03-07 10:17:10 -0500

    force new login for everyone

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Project101::Application.config.secret_token = 'ff03d126d615ae121212d924f16d2e6092444e5a72d1095e0549d31312ccaed24fb8d35b48a420e856a6d5feb7227d68b552d81fca3d26ea9fdd637d4836b2a9'
+Project101::Application.config.secret_token = 'd3416aa63b4bf06ea51b3d71f7cc4eb09fbfd0a61d322c5ca6d8281641ce634145a42c0f85c3c07f0bcd17205bb11088c8a8533a49b09fb0f169b285ff1f8cb5'
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -4,7 +4,7 @@
 
 # Disabled :domain => :all because after 3.0.20 from 3.0.3 upgrade, :cookie_store is broken if 
 #    Subdomain is not specifically set in URL
-Project101::Application.config.session_store :cookie_store, :key => '_project101_session'
+Project101::Application.config.session_store :cookie_store, :key => '_autoins_app_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
